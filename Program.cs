2465f87e3753a7a6b7ea44acfeb7a2d77647136e
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 22:48:03 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ByronCalderon3A_Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resultado_triangulo resultado_triangulo = new Resultado_triangulo();
+            resultado_triangulo.Imprimir();
+        }
+    }
+}
